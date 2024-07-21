a19a0d6b3e819912cfdf13221c450bc6e63c9120
Rohan Ingole <rohaningole2402@gmail.com>
2024-07-21 13:31:51 +0530

    287.Find the Duplicate Number

--- Find_the_Duplicate_Number.java
+++ Find_the_Duplicate_Number.java
@@ -1,4 +1,4 @@
-/*Find the Duplicate Number.cpp
+/*Find_the_Duplicate_Number.java
 Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
 There is only one repeated number in nums, return this repeated number.
@@ -35,22 +35,21 @@ How can we prove that at least one duplicate number must exist in nums?
 Can you solve the problem in linear runtime complexity
 */
 class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        int d=0;
-        int n=nums.size()-1;
-        for(int i=0;i<=n;i++){
-            int t=abs(nums[i])-1;
-            if(nums[t]<0){
-                d=abs(nums[i]);
+    public int findDuplicate(int[] nums) {
+        int d = 0;
+        int n = nums.length - 1;
+        for (int i = 0; i <= n; i++) {
+            int t = Math.abs(nums[i]) - 1;
+            if (nums[t] < 0) {
+                d = Math.abs(nums[i]);
                 break;
-            }else{
-                nums[t]=-nums[t];
+            } else {
+                nums[t] = -nums[t];
             }
         }
-        for(int i=0;i<=n;i++){
-            nums[i]=abs(nums[i]);
+        for (int i = 0; i <= n; i++) {
+            nums[i] = Math.abs(nums[i]);
         }
-        return d;
-    }
-};
+        return d;
+    }
+}
