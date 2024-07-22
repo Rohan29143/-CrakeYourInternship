317a83116187aa25f3fd1b4cce3d9acc9e1e9431
Rohan Ingole <rohaningole2402@gmail.com>
2024-07-22 19:16:20 +0530

    Chocolate Distribution Problem

---
+++ Chocolate Distribution Problem.java
@@ -0,0 +1,86 @@
+/*Chocolate Distribution Problem
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 9 - 3 = 6 by choosing following M packets :{3, 4, 9, 7, 9}.
+Example 2:
+
+Input:
+N = 7, M = 3
+A = {7, 3, 2, 4, 9, 12, 56}
+Output: 2
+Explanation: The minimum difference between maximum chocolates and minimum chocolates is 4 - 2 = 2 by choosing following M packets :{3, 2, 4}.
+*/
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        
+        
+        Collections.sort(a);
+        
+    
+        if (n < m) {
+            return -1;
+        }
+        
+        
+        int minDiff = Integer.MAX_VALUE;
+        
+        for (int i = 0; i + m - 1 < n; i++) {
+            int diff = a.get(i + m - 1) - a.get(i);
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+        
+        return minDiff;
+    }
+}
